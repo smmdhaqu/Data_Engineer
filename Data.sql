7e655c50057ece5e58799e50ec5da36042c48128
Shams Raaju <shraju.db@gmail.com>
2025-05-25 00:09:30 +0200

    SQL, MANY TO MANY Join is easy

--- Data.sql
+++ Data.sql
@@ -36,6 +36,9 @@ select concat("I love to read", ' ', upper(title), ' !!!') from books;
 select left(author_lname, 1) as Last_Name from books;
 select substring(author_lname, 1, 1) as New_Last_Name from books;
 select trim(both '.' from '.....Shams...');
+
+
+-- ===========================================================================
 select database();
 show databases;
 use library;
@@ -218,7 +221,7 @@ SELECT author_fname, author_lname,
 FROM books
 WHERE author_lname IS NOT NULL
 GROUP BY author_fname, author_lname;
-
+-- =====================================================================
 create database shop;
 use shop;
 show databases;
@@ -341,3 +344,90 @@ select first_name, last_name, ifnull(sum(amount), 0) as Total_Money from custome
 left join orders
 on customers.id = orders.customer_id
 group by first_name, last_name;
+
+-- ==================================================================================================
+-- Many to Many
+create database tv_series;
+use tv_series;
+CREATE TABLE reviewers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+CREATE TABLE series (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(100),
+    released_year YEAR,
+    genre VARCHAR(100)
+);
+CREATE TABLE reviews (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    rating DECIMAL(2 , 1 ),
+    series_id INT,
+    reviewer_id INT,
+    FOREIGN KEY (series_id)
+        REFERENCES series (id),
+    FOREIGN KEY (reviewer_id)
+        REFERENCES reviewers (id)
+);
+INSERT INTO series (title, released_year, genre) VALUES
+    ('Archer', 2009, 'Animation'),
+    ('Arrested Development', 2003, 'Comedy'),
+    ("Bob's Burgers", 2011, 'Animation'),
+    ('Bojack Horseman', 2014, 'Animation'),
+    ("Breaking Bad", 2008, 'Drama'),
+    ('Curb Your Enthusiasm', 2000, 'Comedy'),
+    ("Fargo", 2014, 'Drama'),
+    ('Freaks and Geeks', 1999, 'Comedy'),
+    ('General Hospital', 1963, 'Drama'),
+    ('Halt and Catch Fire', 2014, 'Drama'),
+    ('Malcolm In The Middle', 2000, 'Comedy'),
+    ('Pushing Daisies', 2007, 'Comedy'),
+    ('Seinfeld', 1989, 'Comedy'),
+    ('Stranger Things', 2016, 'Drama');
+
+INSERT INTO reviewers (first_name, last_name) VALUES
+    ('Thomas', 'Stoneman'),
+    ('Wyatt', 'Skaggs'),
+    ('Kimbra', 'Masters'),
+    ('Domingo', 'Cortes'),
+    ('Colt', 'Steele'),
+    ('Pinkie', 'Petit'),
+    ('Marlon', 'Crafford');
+
+INSERT INTO reviews(series_id, reviewer_id, rating) VALUES
+    (1,1,8.0),(1,2,7.5),(1,3,8.5),(1,4,7.7),(1,5,8.9),
+    (2,1,8.1),(2,4,6.0),(2,3,8.0),(2,6,8.4),(2,5,9.9),
+    (3,1,7.0),(3,6,7.5),(3,4,8.0),(3,3,7.1),(3,5,8.0),
+    (4,1,7.5),(4,3,7.8),(4,4,8.3),(4,2,7.6),(4,5,8.5),
+    (5,1,9.5),(5,3,9.0),(5,4,9.1),(5,2,9.3),(5,5,9.9),
+    (6,2,6.5),(6,3,7.8),(6,4,8.8),(6,2,8.4),(6,5,9.1),
+    (7,2,9.1),(7,5,9.7),
+    (8,4,8.5),(8,2,7.8),(8,6,8.8),(8,5,9.3),
+    (9,2,5.5),(9,3,6.8),(9,4,5.8),(9,6,4.3),(9,5,4.5),
+    (10,5,9.9),
+    (13,3,8.0),(13,4,7.2),
+    (14,2,8.5),(14,3,8.9),(14,4,8.9);
+
+select * from reviews;
+
+select title from series;
+
+select title, rating from series
+join reviews
+on series.id = reviews.reviewer_id
+order by rating desc
+limit 3;
+
+SELECT 
+    title, ROUND(AVG(rating), 2) AS Ave_Rating
+FROM
+    series
+        JOIN
+    reviews ON series.id = reviews.reviewer_id
+GROUP BY title
+order by Ave_Rating asc;
+
+SELECT first_name, last_name, rating from reviewers
+join reviews
+on reviewers.id = reviews.reviewer_id;
