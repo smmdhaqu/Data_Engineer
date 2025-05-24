6dbe84d0e5f7009a347943faead1819628d99723
Shams Raaju <shraju.db@gmail.com>
2025-05-24 22:11:46 +0200

    SQL, LEFT JOIN done

--- Data.sql
+++ Data.sql
@@ -287,3 +287,57 @@ FROM
     orders o ON c.id = o.customer_id
 ORDER BY o.amount DESC
 LIMIT 1;
+select * from orders
+join customers 
+on customers.id = orders.customer_id;
+
+select * from customers
+join orders 
+on orders.customer_id = customers.id;
+drop table orders;
+drop tables customers;
+show tables;
+select * from orders;
+create database shops;
+select database();
+use shops;
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
+       
+select * from customers
+join orders 
+on orders.customer_id = customers.id;
+
+select first_name, last_name, order_date, amount from customers
+left join orders
+on customers.id = orders.customer_id;
+
+select first_name, last_name, ifnull(sum(amount), 0) as Total_Money from customers
+left join orders
+on customers.id = orders.customer_id
+group by first_name, last_name;
