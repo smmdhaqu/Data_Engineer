dc6e51de096637db6fca63c5eab6dcfc11167f1a
Shams Raaju <shraju.db@gmail.com>
2025-05-24 21:18:24 +0200

    SQL, Joining is a bit complex

--- Data.sql
+++ Data.sql
@@ -218,3 +218,56 @@ SELECT author_fname, author_lname,
 FROM books
 WHERE author_lname IS NOT NULL
 GROUP BY author_fname, author_lname;
+
+create database shop;
+use shop;
+show databases;
+select database();
+use shop;
+
+
+CREATE TABLE customers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50)
+);
+ 
+CREATE TABLE orders (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+ 
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+       
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
+       
+show tables;
+select id from customers where last_name = 'George';
+select * from orders where customer_id = 1;
+select * from customers, orders;
+show databases;
+select * from customers 
+join orders
+on orders.customer_id = customers.id;
+
+SELECT c.id, c.first_name, c.last_name, c.email, o.id AS order_id, o.order_date, o.amount
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+WHERE c.first_name = 'George' 
+  AND c.last_name = 'Michael' 
+  AND o.order_date = '2014-12-12';
