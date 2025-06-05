5fd5a446e82492837510df6075c05c28d784998f
Shams Raaju <shraju.db@gmail.com>
2025-06-05 12:40:42 +0200

    Overview Variables for R

---
+++ variables2.R
@@ -0,0 +1,19 @@
+num1 <- 10
+num2 <-20
+sum <- num1+num2
+sum
+num3 <- 5.5
+num4 <- 4
+sum2 <- num3 / num4
+sum2
+
+sum3 <- num3 * num4
+sum3
+square <- sqrt(num4)
+square
+
+greating <- "Good Morning"
+name <- "Shams Haque"
+message_show <- paste(greating, name)
+message_show
+
